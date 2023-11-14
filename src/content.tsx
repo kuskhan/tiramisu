05509f562ba40247fa80b18326b0195763d348ed
Jung Woong Park <kuskhan@gmail.com>
2023-11-15 07:48:48 +0900

    remove unused permission request

--- src/content.tsx
+++ src/content.tsx
@@ -18,9 +18,7 @@ const PlasmoOverlay = () => {
   useEffect(() => {
     chrome.storage.local.get(["switch"]).then((result) => {
       if (result.switch) {
-        // const fontUrlKo = chrome.runtime.getURL("fonts/nanum-pretty-mk.ttf")
         const fontUrlKo = chrome.runtime.getURL("fonts/SSFaithfulness.ttf")
-
         const fontUrlJp = chrome.runtime.getURL("fonts/KazuFont.ttf")
         document.head.insertAdjacentHTML(
           "beforeend",
@@ -31,14 +29,12 @@ const PlasmoOverlay = () => {
   }, [])
 
   useEffect(() => {
-    chrome.runtime.onMessage.addListener(
-      function (request, sender, sendMessage) {
-        if (request.message === "refresh") {
-          // document.location.reload()
-          window.location.reload()
-        }
+    chrome.runtime.onMessage.addListener(function (request) {
+      if (request.message === "refresh") {
+        document.location.reload()
+        // window.location.reload()
       }
-    )
+    })
   }, [])
 
   useEffect(() => {}, [])
